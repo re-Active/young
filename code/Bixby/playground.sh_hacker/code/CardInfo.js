df7c27a514ca34bf5ed12d13923d68afac6e7561
young31 <migael38317@gmail.com>
2019-11-23 02:39:17 +0900

    cardinfo

--- code/Bixby/playground.sh_hacker/code/CardInfo.js
+++ code/Bixby/playground.sh_hacker/code/CardInfo.js
@@ -1,4 +1,38 @@
-module.exports.function = function cardinfo (cardinfoinput) {
-  
-  return {}
+module.exports.function = function cardinfo(cardinfoinput) {
+  const db = require('./fake/fakedb').card
+  const console = require('console')
+  const http = require('http')
+  const target = '/v1/mycard/searchavailablecard'
+  const cardUrl = 'http://10.3.17.61:8080'
+  const params = {
+    "dataHeader": {
+    },
+    "dataBody": {
+      "nxtQyKey": ""
+    }
+  }
+
+  let data
+  try {
+    data = http.postUrl(cardUrl + target, params, { format: 'json' })
+  } catch (e) {
+    console.log(e)
+  } finally {
+    data = db
+  }
+
+  cardinfoset = []
+  for(i in data){
+    temp = {
+      category: data[i].category,
+      name: data[i].name,
+      cardno: data[i].cardno
+    }
+
+    cardinfoset.push(temp)
+
+  }
+  return {
+    cardinfoset: cardinfoset
+  }
 }
--- code/Bixby/playground.sh_hacker/code/fake/fakedb.js
+++ code/Bixby/playground.sh_hacker/code/fake/fakedb.js
@@ -5,16 +5,29 @@ module.exports.bank = [
     balance: '3,000,000',
     accountno: '122-233'
   },
-    {
+  {
     category: '정기적금',
     name: 'fakeInstallment',
     balance: '25,232,384',
     accountno: '233-455'
   },
-    {
+  {
     category: '자유입출금',
     name: 'fakeDeposit',
     balance: '523,248',
     accountno: '788-889'
   }
+]
+
+module.exports.card = [
+  {
+    category: '체크카드',
+    name: 'fakeCheck',
+    cardno: '123-456-789',
+  },
+  {
+    category: '신용카드',
+    name: 'fakeCredit',
+    cardno: '456-789-123',
+  },
 ]
