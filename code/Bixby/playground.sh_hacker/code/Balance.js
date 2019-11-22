41d572c9ae8ab7245be97b3368a2a31852f17e8d
young31 <migael38317@gmail.com>
2019-11-23 02:12:50 +0900

    test

--- code/Bixby/playground.sh_hacker/code/Balance.js
+++ code/Bixby/playground.sh_hacker/code/Balance.js
@@ -1,5 +1,44 @@
-module.exports.function = function balance (balanceinput) {
+module.exports.function = function balance(balanceinput) {
+  const db = require('./fake/fakedb').bank
+  const console = require('console')
   const http = require('http')
+  const target = '/v1/account/list'
+  const bankUrl = 'http://10.3.17.61:8080'
+  const params = {
+    "dataHeader":
+      {},
+    "dataBody":
+    {
+      "serviceCode": "C2010",
+      "거래구분": "9",
+      "계좌감추기여부": "1",
+      "보안계좌조회구분": "2",
+      "주민등록번호": "WmokLBDCO9/yfihlYoJFyg=="
+    }
+  }
+  let data
+  try {
+    data = http.postUrl(bankUrl + target, params, { format: 'json' })
+  } catch(e) {
+    console.log(e)
+  } finally {
+    data = db
+  }
 
-  return {}
+  balanceset = []
+  for(i in data){
+    temp = {
+      category: data[i].category,
+      name: data[i].name,
+      balance: data[i].balance,
+      accountno: data[i].accountno
+    }
+
+    balanceset.push(temp)
+
+  }
+
+  return {
+    balanceset: balanceset
+  }
 }
---
+++ code/Bixby/playground.sh_hacker/code/fake/fakedb.js
@@ -0,0 +1,20 @@
+module.exports.bank = [
+  {
+    category: '정기예금',
+    name: 'fakeTime',
+    balance: '3,000,000',
+    accountno: '122-233'
+  },
+    {
+    category: '정기적금',
+    name: 'fakeInstallment',
+    balance: '25,232,384',
+    accountno: '233-455'
+  },
+    {
+    category: '자유입출금',
+    name: 'fakeDeposit',
+    balance: '523,248',
+    accountno: '788-889'
+  }
+]
